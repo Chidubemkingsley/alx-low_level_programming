a7c2a4ee4826abed288c9571a94c3a7cf959252d
Chidubemkingsley <rubelu2017@gmail.com>
2023-12-11 15:49:23 -0800

    that addc numbers in the integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add -> programmed to adds two number
+* @num1 : param1 of the particular integer
+* @num2 : param2 of the particular integer
+* Return: x + y
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
