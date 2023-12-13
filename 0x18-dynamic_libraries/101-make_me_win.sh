5117638bb3278c72e3da0449c70887e32347dd60
Chidubemkingsley <rubelu2017@gmail.com>
2023-12-13 15:50:37 -0800

    last hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Chidubemkingsley/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P /tmp/ https://raw.github.com/Chidubemkingsley/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
